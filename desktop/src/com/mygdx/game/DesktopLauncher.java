e56fe83613ddfbfca3b0610e8e51c7cabba3e71f
lei.zhou <lei.zhou@derbysoft.net>
2023-10-21 16:21:47 +0800

    feat: add touch pad

--- desktop/src/com/mygdx/game/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/DesktopLauncher.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.mygdx.game.animation.Animator;
 import com.mygdx.game.physics.MyBox2DImgWorld;
 import com.mygdx.game.physics.MyBox2DWorld;
+import com.mygdx.game.touch.TouchpadTest;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
@@ -13,6 +14,6 @@ public class DesktopLauncher {
 		config.setForegroundFPS(60);
 		config.setTitle("My GDX Game");
 
-		new Lwjgl3Application(new MyBox2DImgWorld(), config);
+		new Lwjgl3Application(new TouchpadTest(), config);
 	}
 }
